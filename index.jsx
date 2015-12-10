061225e3d26fa8c4cc60cf0f37d52b30291c81e3
nezriffic <nezriffic@mlask.com>
2015-12-10 10:55:12 +0100

    npm preparation

---
+++ index.jsx
@@ -0,0 +1,138 @@
+/**
+ * Edit in place React Component
+ *
+ * @author: Michal Sekula
+ */
+
+var React = require('react'),
+
+    EditInPlace = React.createClass({
+
+        MODES: {
+            read: 0,
+            edit: 1
+        },
+
+        propTypes: {
+            validate: React.PropTypes.func,
+            onChange: React.PropTypes.func,
+            text: React.PropTypes.string.isRequired,
+            maxLength: React.PropTypes.number,
+            editType: React.PropTypes.string,
+            className: React.PropTypes.string,
+            activeClassName: React.PropTypes.string
+        },
+
+        getInitialState: function () {
+
+            if (this.props.validate) {
+                this._validate = this.props.validate;
+            } else {
+                this._validate = function () {
+                    return true;
+                };
+            }
+
+            return {
+                text: this.props.text,
+                mode: this.MODES.read
+            };
+
+        },
+
+        _onCancel: function () {
+            this.setState({
+                mode: this.MODES.read
+            });
+
+        },
+
+        _onKeyDown: function (e) {
+            if (e && e.keyCode === 27) {
+                this._onCancel();
+            }
+
+        },
+
+        componentDidMount: function () {
+            window.addEventListener('keydown', this._onKeyDown);
+
+        },
+
+        componentWillUnmount: function () {
+            window.removeEventListener('keydown', this._onKeyDown);
+
+        },
+
+        _onDoubleClick: function (e) {
+            e.preventDefault();
+
+            if (this.state.mode === this.MODES.read) {
+                this.setState({
+                    mode: this.MODES.edit
+                }, function () {
+                    React.findDOMNode(this.refs.input).focus();
+                    // to set cursor at the end
+                    React.findDOMNode(this.refs.input).value = React.findDOMNode(this.refs.input).value;
+                });
+            }
+
+        },
+
+        _onClick: function (e) {
+            this._onDoubleClick(e);
+
+        },
+
+        _onSave: function (e) {
+            e.preventDefault();
+
+            var node = React.findDOMNode(this.refs.input);
+
+            if (node.value === this.state.text) {
+                this.setState({
+                    mode: this.MODES.read
+                });
+            } else if (this._validate(node.value)) {
+                this.setState({
+                    text: node.value,
+                    mode: this.MODES.read
+                }, function () {
+                    if (this.props.onChange) {
+                        this.props.onChange(this.state.text);
+                    }
+                });
+            } else {
+                node.focus();
+            }
+
+        },
+
+        render: function () {
+            var classname = this.props.className || 'editinplace',
+                activeclassname = this.props.activeClassName || 'active';
+
+            if (this.state.mode === this.MODES.edit) {
+                return <div className={classname + ' ' + activeclassname}>
+                    <form onSubmit={this._onSave}>
+                        <input ref="input"
+                               type="text"
+                               defaultValue={this.state.text}
+                               maxLength={this.props.maxLength}
+                               onBlur={this._onSave} />
+                    </form>
+                </div>;
+
+            }
+
+            if (this.props.editType && this.props.editType.toLowerCase() === 'dblclick') {
+                return <div className={classname} onDoubleClick={this._onDoubleClick}>{this.state.text}</div>;
+
+            }
+
+            return <div className={classname} onClick={this._onClick}>{this.state.text}</div>;
+
+        }
+    });
+
+module.exports = EditInPlace;
